56fec9e8f588b0314657127e9fa957288510c166
Casmir Okorie <casmir456@gmail.com>
2024-05-25 21:02:54 +0100

    add service page

--- js/slider.js
+++ js/slider.js
@@ -6,10 +6,10 @@ document.addEventListener('DOMContentLoaded', () => {
     slidesPerGroup: 1,
     loop: true,
 
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-    },
+    // autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false,
+    // },
     loopFillGroupWithBlank: true,
     pagination: {
         el: '.swiper-pagination',

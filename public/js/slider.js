bc8749f4f009a5dac8d0734861f58156e26421e4
Casmir Okorie <casmir456@gmail.com>
2024-08-14 11:34:47 +0100

    added some pages

--- public/js/slider.js
+++ public/js/slider.js
@@ -33,3 +33,42 @@
 //     });
 // });
 /// what our customer are saying slider code////
+
+
+// logos for our_trusted_partners
+document.addEventListener('DOMContentLoaded', () => {
+    var swiper = new Swiper(".our_partners-container ", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 1,
+        loop: true,
+        // autoplay: {
+        //     delay: 5000,
+        //     disableOnInteraction: false,
+        // },
+        loopFillGroupWithBlank: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".more_logo_next",
+            prevEl: ".less_logo_prev",
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 4,
+            },
+            520: {
+                slidesPerView: 6,
+            },
+            950: {
+                slidesPerView: 8,
+            },
+        },
+    });
+    
+    
+});
+
+// logos for our_trusted_partners ends
